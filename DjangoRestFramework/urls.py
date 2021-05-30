e4d15ded9cce4a9e38666a672c58b8e6516dec23
Denis <pda-gb>
2021-05-30 20:17:16 +0300

    Revert "rtrtrt"
    This reverts commit e15120e9cfe6f447f1af867280f5b9eb627a04c8.

--- DjangoRestFramework/urls.py
+++ DjangoRestFramework/urls.py
@@ -4,7 +4,6 @@ The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.2/topics/http/urls/
 Examples:
 Function views
-koijjiojoijoi
     1. Add an import:  from my_app import views
     2. Add a URL to urlpatterns:  path('', views.home, name='home')
 Class-based views
